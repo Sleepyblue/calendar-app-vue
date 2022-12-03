3b30e557310d6f9b47a013dc141739a7b33e38d6
Bruno Xavier <69484045+Sleepyblue@users.noreply.github.com>
2022-12-03 14:16:07 +0000

    🧬 Creation of an 'utils' file
    Why
    To store the logic of week creation and control so it's easier to share the data between components
    
    How
    - Creating of a 'utils.ts' file
    - Exporting each function
    
    Tags
    On branch days
    Changes to be committed:
     modified:   src/components/Days.vue
     new file:   src/components/utils.ts

--- src/components/Days.vue
+++ src/components/Days.vue
@@ -4,39 +4,7 @@
 
 <script lang="ts" setup>
 import Day from './Day.vue';
-
-function getCurrentWeekDates(currentDate = new Date()) {
-  let currentWeek = [];
-  for (let i = 0; i <= 6; i++) {
-    const currentDay = currentDate.getDay() === 0 ? 7 : currentDate.getDay();
-    const day = currentDate.setDate(currentDate.getDate() - currentDay + i + 1);
-    currentWeek.push(new Date(day).toISOString().slice(0, 10));
-  }
-  return currentWeek;
-}
-
-function convertToReadableWeekDates(currentWeekArray: string[]) {
-  let currentReadableWeek: string[] = [];
-  const options: {} = {
-    weekday: 'long',
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  };
-
-  currentWeekArray.forEach((date) => {
-    const readableDay = new Date(date).toLocaleDateString('en-GB', options);
-    currentReadableWeek.push(readableDay);
-  });
-  return currentReadableWeek;
-}
+import { getCurrentWeekDates } from './utils';
 
 const weekView = getCurrentWeekDates();
-
-// TODO: Initial brainstorming for week change
-const today = new Date();
-// const lastWeek = new Date(today.setDate(today.getDate() - 7)).toISOString();
-const lastWeek = new Date(today.setDate(today.getDate() - 7));
 </script>
-
-<style></style>
