4a18199672a4e6d54c8f0144fd6dbe7d57cd9f22
Bruno Xavier <69484045+Sleepyblue@users.noreply.github.com>
2022-12-04 17:00:10 +0000

    🌈 Add a background grid to week view
    Why
    So it's easier to the user to visually identify each hour limits
    
    How
    - Add a background on each day to give that illusion
    - Minor styling tweaks to better accommodate the change
    
    Tags
    Changes to be committed:
     modified:   src/components/Day.vue
     modified:   src/components/DayHeader.vue
     modified:   src/components/Days.vue

--- src/components/DayHeader.vue
+++ src/components/DayHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-pink-300 top-0 left-0 sticky h-[5%]">{{ date }}</div>
+  <div class="bg-pink-300 top-0 left-0 sticky h-[10%]">{{ date }}</div>
 </template>
 
 <script lang="ts" setup>
