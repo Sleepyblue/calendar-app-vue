292cbf055decc59954adb8ec63c2133fc293f5e7
Bruno Xavier <69484045+Sleepyblue@users.noreply.github.com>
2022-12-08 15:09:59 +0000

    🐞🌈 Fix day header moving on scroll
    Why
    The day header would move and hide on the last stretch of scrolling when viewing late night hours
    
    How
    - Give full-height to both 'Day.vue'
    - Remove the 'height' styling from 'DayHeader.vue'
    - Removing unnecessary CSS to achieve a visual background grid
    - Converting to 'Tailwind' the necessary classes (except the linear gradient)
    - Renaming the 'back_grid' to 'back_gradient-grid'
    
    Tags
    On branch back_grid
    Changes to be committed:
     modified:   src/components/Day.vue
     modified:   src/components/DayHeader.vue
     modified:   src/components/Days.vue

--- src/components/Day.vue
+++ src/components/Day.vue
@@ -1,18 +1,14 @@
 <template>
   <div
     ref="day"
-    class="bg-amber-500 m-[1px] grid grid-rows-[repeat(24,_minmax(3em,_1fr))] grid-cols-1 border-t-black back-grid"
+    class="bg-amber-500 m-[1px] grid grid-rows-[repeat(24,_minmax(3em,_1fr))] grid-cols-1 border-t-black bg-[length:100%_48.8281px] back_gradient-grid h-full"
   ></div>
 </template>
 
 <script lang="ts" setup></script>
 
 <style scoped>
-.back-grid {
-  background-position-y: 8em;
-  background-size: 100% calc(1em + 30px);
-  background-position: top;
-  background-repeat: repeat-y;
+.back_gradient-grid {
   background-image: linear-gradient(
     to bottom,
     black 0%,
--- src/components/Days.vue
+++ src/components/Days.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-for="(days, index) in weekView" class="min-h-fit">
+  <div v-for="(days, index) in weekView">
     <DayHeader :date="readableWeekDate[index]" class="z-10" />
     <Day class="z-0" />
   </div>
