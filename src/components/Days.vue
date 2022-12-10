c315561203fa6dee1f14df86c1a197165c64c814
Bruno Xavier <69484045+Sleepyblue@users.noreply.github.com>
2022-12-10 15:51:53 +0000

    ✨ Adding calendar hours display
    Why
    To have a visual hour indication next to the calendar days
    
    How
    - Moved the `div` containing the grid days formatting to the `Days.vue` component to avoid a `fragment`
    - Created the `Hours.vue` component to control the calendar's hours display
    - Re-used the `Days.vue` and `DayHeader.vue` components to generate the `Hours.vue` container and maintain the grid formatting
    - Added a named `slots` 'hours' to generate the `Hours.vue` items (temporary implementation)
    
    Tags
    On branch hours
    Changes to be committed:
        - modified:   src/components/Day.vue
        - modified:   src/components/Days.vue
        - new file:   src/components/Hours.vue
        - modified:   src/views/Calendar.vue

--- src/components/Days.vue
+++ src/components/Days.vue
@@ -1,13 +1,26 @@
 <template>
-  <div v-for="(days, index) in weekView">
-    <DayHeader :date="readableWeekDate[index]" class="z-10" />
-    <Day class="z-0" />
+  <div
+    class="row-start-3 row-end-[12] bg-blue-500 col-start-3 col-end-[12] grid-cols-8 grid overflow-y-scroll overflow-x-hidden relative"
+  >
+    <div class="h-full">
+      <DayHeader class="z-10" />
+      <Day class="z-0">
+        <template #hours>
+          <Hours v-for="(hour, index) in hours" :hour="index + 1" />
+        </template>
+      </Day>
+    </div>
+    <div v-for="(days, index) in weekView">
+      <DayHeader :date="readableWeekDate[index]" class="z-10" />
+      <Day class="z-0" />
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import Day from './Day.vue';
 import DayHeader from '../components/DayHeader.vue';
+import Hours from './Hours.vue';
 import {
   getCurrentWeekDates,
   convertToReadableWeekDates,
@@ -16,5 +29,6 @@ import {
 const currentWeek = getCurrentWeekDates();
 const readableWeekDate = convertToReadableWeekDates(currentWeek);
 
+const hours = 23;
 const weekView = getCurrentWeekDates();
 </script>
--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -2,11 +2,7 @@
   <div
     class="w-full h-full grid grid-cols-12 grid-rows-[repeat(12,minmax(0,1fr))] bg-[#f8673b] overflow-hidden"
   >
-    <div
-      class="row-start-3 row-end-[12] bg-blue-500 col-start-4 col-end-[12] grid-cols-7 grid overflow-y-scroll overflow-x-hidden relative"
-    >
-      <Days />
-    </div>
+    <Days />
     <Sidebar />
   </div>
 </template>
