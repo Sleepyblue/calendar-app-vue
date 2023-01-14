8fabadf7de5faa4a32e8a49a16722d713107f6ee
Bruno Xavier <69484045+Sleepyblue@users.noreply.github.com>
2023-01-14 16:26:44 +0000

    🌈✨ Styling Overhaul 3 (Fixing background grid limits) and adding 'EventHeader' component
    Why
    - To fix the background top limit showing
    - To help with full-day events
    
    How
    - Adding the 'EventHeader' component helped hidding the top background limit
    
    Tags
     On branch styling_overhaul
     Changes to be committed:
    	modified:   src/components/Day.vue
    	modified:   src/components/DayHeader.vue
    	new file:   src/components/EventHeader.vue

--- src/components/DayHeader.vue
+++ src/components/DayHeader.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class="top-0 left-0 sticky h-[5%] mx-[2px] bg-white">
+  <div class="top-0 left-0 sticky h-[5%] px-1 bg-white">
     <span class="text-black">{{ date }}</span>
     <span
       v-if="!isBlank"
       class="w-11/12 h-[3px] rounded-full bg-gray-200 bottom-0 left-0 absolute"
     ></span>
+    <EventHeader v-if="!isBlank" />
   </div>
 </template>
 
 <script lang="ts" setup>
+import EventHeader from './EventHeader.vue';
+
 const props = defineProps({
   isBlank: Boolean,
   date: String,
---
+++ src/components/EventHeader.vue
@@ -0,0 +1,6 @@
+<template>
+  <div
+    class="w-full min-h-[4px] absolute left-0 bottom-0 translate-y-full bg-white"
+  ></div>
+</template>
+<script setup lang="ts"></script>
