2182c78131661284156501941d62258d19377713
Bruno Xavier <69484045+Sleepyblue@users.noreply.github.com>
2023-06-02 17:37:49 +0100

    🧬 Refactoring `DefaultLayout` and experimenting with `Global Keyboard Shortcuts`
    Why
    - Continuing the project code review
    
    How
    - Added icons to convey the `Sidebar` collapsed and expanded
    - Removed the `Dot` icon
    - Refactoring the `DefaultLayout` to see how to properly implement the desired layout
    - Experimenting with `Global Keyboard Shortcuts`
    
    Tags
     Changes to be committed:
    	deleted:    src/assets/icons/Dot.vue
    	new file:   src/assets/icons/SidebarCollapse.vue
    	new file:   src/assets/icons/SidebarExpand.vue
    	new file:   src/components/organisms/Header/Header.vue
    	new file:   src/components/organisms/Header/index.ts
    	modified:   src/components/organisms/Sidebar/Sidebar.vue
    	modified:   src/views/pages/DefaultLayout/DefaultLayout.vue

---
+++ src/assets/icons/SidebarExpand.vue
@@ -0,0 +1,16 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <g
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+    >
+      <path
+        d="M4 6a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm5-2v16"
+      />
+      <path d="m14 10l2 2l-2 2" />
+    </g>
+  </svg>
+</template>
