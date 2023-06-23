9f1a872a7b75cab3a5effdca8b2474e09401c77a
Bruno Xavier <69484045+Sleepyblue@users.noreply.github.com>
2023-06-23 17:01:20 +0100

    ⭐ Adding a preview on `Event` click
    Why
    - To add a preview of target `Event` after clicking on the `EventCard`
    
    How
    - Removed the `grid-start` class on the `EventCard` atom since it was unnecessary
    - Created a new `EventDisplay` component to handle the `Event` data display on click instead of directly calling the `EventModal`
    - Made so the `EventDisplay` was controlled on the `Calendar` parent component through `emits`
    - Temporarily disabled the `EventModal` edit variant
    - Moved the `EventModal`logic into the `EventDisplay` component (will handle it later)
    - Made so the `EventDisplay` updated its position on `resize` and `scroll`
    - Added conditional control over the `Event Listeners` which update the `EventDisplay` position on `scroll` and `resize`. They're only needed if the `EventDisplay` is shown
    
    Tags
     Changes to be committed:
    	new file:   src/assets/icons/Close.vue
    	modified:   src/components/atoms/EventCard/EventCard.vue
    	new file:   src/components/atoms/EventDisplay/EventDisplay.vue
    	new file:   src/components/atoms/EventDisplay/index.ts
    	modified:   src/components/molecules/CalendarDay/CalendarDay.vue
    	modified:   src/components/organisms/Calendar/Calendar.vue

--- src/components/atoms/EventCard/EventCard.vue
+++ src/components/atoms/EventCard/EventCard.vue
@@ -1,36 +1,24 @@
 <template>
   <article
-    class="event-card grid-start flex w-11/12 flex-col items-start justify-start gap-1 overflow-hidden rounded-md px-2 py-1 text-white"
+    class="event-card flex w-11/12 flex-col items-start justify-start gap-1 rounded-md px-2 py-1 text-white"
     :class="{ 'grid-end': eventEndHour }"
-    @click="show = true"
+    @click="$emit('openEventDisplay', $event)"
   >
     <p
-      class="w-full shrink-0 truncate whitespace-normal text-xs font-bold"
+      class="pointer-events-none w-full shrink-0 truncate whitespace-normal text-xs font-bold"
       :class="isShort ? 'whitespace-nowrap' : 'line-clamp-2'"
     >
       {{ eventTitle }}
     </p>
-    <p class="text-xs font-light">
+    <p class="pointer-events-none text-xs font-light">
       {{ hourRange }}
     </p>
-    <EventModal
-      v-if="show"
-      :show="show"
-      :id="eventId"
-      :title="eventTitle"
-      :date="eventDate"
-      :startHour="eventStartHour"
-      :endHour="eventEndHour"
-      @close="show = false"
-      edit
-    />
   </article>
 </template>
 
 <script lang="ts" setup>
 import { ref, computed } from 'vue';
 import { formatHoursRange } from '@/utils/Hours';
-import EventModal from '@/components/atoms/EventModal';
 
 const { eventStartHour, eventEndHour } = defineProps<{
   eventDate: string;
@@ -52,17 +40,14 @@ const isShort = computed(() =>
 </script>
 
 <style>
-.grid-start {
+.event-card {
+  height: calc(100% - 2px);
+  background-color: #f5a278;
+  box-shadow: rgba(0, 0, 0, 0.15) 2.4px 2.4px 3.2px;
   grid-row-start: v-bind('eventStartHour + 1');
 }
 
 .grid-end {
   grid-row-end: v-bind('eventEndHour + 1');
-}
-
-.event-card {
-  height: calc(100% - 2px);
-  background-color: #f5a278;
-  box-shadow: rgba(0, 0, 0, 0.15) 2.4px 2.4px 3.2px;
 }
 </style>
